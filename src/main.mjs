2dc37625b1c0a20a19a7ffc398627106883a92be
Johannes Ewald <johannes.ewald@peerigon.com>
2020-03-17 21:42:55 +0100

    Finish build setup

---
+++ src/main.mjs
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-unresolved */
+import mainExports from "./main.js";
+
+export const parseDomain = mainExports.parseDomain;
+export const ParseResultType = mainExports.ParseResultType;
+export const ValidationErrorType = mainExports.ValidationErrorType;
