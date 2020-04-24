907003044760b1ca9713ec7d0e48e9bd208daa9d
Johannes Ewald <johannes.ewald@peerigon.com>
2020-04-24 19:36:57 +0200

    fix: npx parse-domain-update broken (#105)
    This commit fixes an error where a non-existent function was called from ./bin/update.js. The smoke test now doesn't execute the test just by requiring the module.
    
    In order to prevent this from happening in the future, the posttest script has been changed so that the update script is executed on each posttest. Since posttest is now updating the tries, we do not need to update them manually before npm publish — npm test is enough.
    
    Fixes https://github.com/peerigon/parse-domain/issues/104

--- src/parse-domain.test.ts
+++ src/parse-domain.test.ts
@@ -465,8 +465,4 @@ describe(parseDomain.name, () => {
 			hostname: "www.EXAMPLE.test",
 		});
 	});
-
-	test("smoke test", () => {
-		require("./smoke-test.ts");
-	});
 });
--- src/smoke-test.ts
+++ src/smoke-test.ts
@@ -1,57 +1,59 @@
 import {deepStrictEqual} from "assert";
 import {parseDomain} from "./main";
 
-deepStrictEqual(parseDomain("www.example.com"), {
-	hostname: "www.example.com",
-	type: "LISTED",
-	labels: ["www", "example", "com"],
-	subDomains: ["www"],
-	domain: "example",
-	topLevelDomains: ["com"],
-	icann: {
+export const runSmokeTest = () => {
+	deepStrictEqual(parseDomain("www.example.com"), {
+		hostname: "www.example.com",
+		type: "LISTED",
+		labels: ["www", "example", "com"],
 		subDomains: ["www"],
 		domain: "example",
 		topLevelDomains: ["com"],
-	},
-});
-deepStrictEqual(parseDomain("www.example.co.uk"), {
-	hostname: "www.example.co.uk",
-	type: "LISTED",
-	labels: ["www", "example", "co", "uk"],
-	subDomains: ["www"],
-	domain: "example",
-	topLevelDomains: ["co", "uk"],
-	icann: {
+		icann: {
+			subDomains: ["www"],
+			domain: "example",
+			topLevelDomains: ["com"],
+		},
+	});
+	deepStrictEqual(parseDomain("www.example.co.uk"), {
+		hostname: "www.example.co.uk",
+		type: "LISTED",
+		labels: ["www", "example", "co", "uk"],
 		subDomains: ["www"],
 		domain: "example",
 		topLevelDomains: ["co", "uk"],
-	},
-});
-deepStrictEqual(parseDomain("www.example.cloudfront.net"), {
-	hostname: "www.example.cloudfront.net",
-	type: "LISTED",
-	labels: ["www", "example", "cloudfront", "net"],
-	subDomains: ["www"],
-	domain: "example",
-	topLevelDomains: ["cloudfront", "net"],
-	icann: {
-		subDomains: ["www", "example"],
-		domain: "cloudfront",
-		topLevelDomains: ["net"],
-	},
-});
-// TODO: Use fromUrl once we dropped Node v8 support
-// deepStrictEqual(parseDomain(fromUrl("www.食狮.公司.cn")), {
-deepStrictEqual(parseDomain("www.xn--85x722f.xn--55qx5d.cn"), {
-	hostname: "www.xn--85x722f.xn--55qx5d.cn",
-	type: "LISTED",
-	labels: ["www", "xn--85x722f", "xn--55qx5d", "cn"],
-	subDomains: ["www"],
-	domain: "xn--85x722f",
-	topLevelDomains: ["xn--55qx5d", "cn"],
-	icann: {
+		icann: {
+			subDomains: ["www"],
+			domain: "example",
+			topLevelDomains: ["co", "uk"],
+		},
+	});
+	deepStrictEqual(parseDomain("www.example.cloudfront.net"), {
+		hostname: "www.example.cloudfront.net",
+		type: "LISTED",
+		labels: ["www", "example", "cloudfront", "net"],
+		subDomains: ["www"],
+		domain: "example",
+		topLevelDomains: ["cloudfront", "net"],
+		icann: {
+			subDomains: ["www", "example"],
+			domain: "cloudfront",
+			topLevelDomains: ["net"],
+		},
+	});
+	// TODO: Use fromUrl once we dropped Node v8 support
+	// deepStrictEqual(parseDomain(fromUrl("www.食狮.公司.cn")), {
+	deepStrictEqual(parseDomain("www.xn--85x722f.xn--55qx5d.cn"), {
+		hostname: "www.xn--85x722f.xn--55qx5d.cn",
+		type: "LISTED",
+		labels: ["www", "xn--85x722f", "xn--55qx5d", "cn"],
 		subDomains: ["www"],
 		domain: "xn--85x722f",
 		topLevelDomains: ["xn--55qx5d", "cn"],
-	},
-});
+		icann: {
+			subDomains: ["www"],
+			domain: "xn--85x722f",
+			topLevelDomains: ["xn--55qx5d", "cn"],
+		},
+	});
+};
