4985cc7d5febe1e1ac98c2a38095e59bd2f54da9
Johannes Ewald <johannes.ewald@peerigon.com>
2022-01-21 15:36:58 +0100

    Format code

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,26 @@
 "use strict";
 
 module.exports = {
-	roots: ["<rootDir>/src"],
-	transform: {
-		"^.+\\.tsx?$": "ts-jest",
-	},
-	globals: {
-		"ts-jest": {
-			diagnostics: false,
-		},
-	},
-	collectCoverage: true,
-	coverageThreshold: {
-		"./src/**/*.ts": {
-			branches: 100,
-			functions: 100,
-			lines: 100,
-			statements: 100,
-		},
-	},
-	coveragePathIgnorePatterns: [
-		"<rootDir>/src/tests",
-		"<rootDir>/node_modules/",
-	],
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      diagnostics: false,
+    },
+  },
+  collectCoverage: true,
+  coverageThreshold: {
+    "./src/**/*.ts": {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  coveragePathIgnorePatterns: [
+    "<rootDir>/src/tests",
+    "<rootDir>/node_modules/",
+  ],
 };
