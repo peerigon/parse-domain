6f9509cd763f951c40fc61a3e717f39393de5fc9
Johannes Ewald <johannes.ewald@peerigon.com>
2023-10-27 17:59:21 +0200

    chore: Update dependencies
    The breaking change in is-ip is only about the support Node version. They dropped support for Node 14 which we also don't support anymore.

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,16 @@
 export default {
   roots: ["<rootDir>/src"],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
   },
   extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
-  },
-  globals: {
-    "ts-jest": {
-      useESM: true,
-    },
   },
   collectCoverage: true,
   coverageThreshold: {
